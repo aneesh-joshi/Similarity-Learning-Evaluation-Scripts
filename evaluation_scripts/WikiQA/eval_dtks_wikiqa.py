bf414aaf013f0f388e915d8c47a471027d1a8c49
Aneesh <aneeshyjoshi@gmail.com>
2018-08-04 12:45:37 +0000

    finish mp wiki with 0.61

--- evaluation_scripts/WikiQA/eval_dtks_wikiqa.py
+++ evaluation_scripts/WikiQA/eval_dtks_wikiqa.py
@@ -9,7 +9,7 @@ from utils import MyWikiIterable
 model_save_path = 'saved_models'
 model_name = 'dtks_wikiqa_model'
 
-wikiqa_folder = '..', '..', 'data', 'WikiQACorpus'
+wikiqa_folder = os.path.join('..', '..', 'data', 'WikiQACorpus')
 
 q_iterable = MyWikiIterable('query', os.path.join(wikiqa_folder, 'WikiQA-train.tsv'))
 d_iterable = MyWikiIterable('doc', os.path.join(wikiqa_folder, 'WikiQA-train.tsv'))
@@ -23,13 +23,15 @@ q_test_iterable = MyWikiIterable('query', os.path.join(wikiqa_folder, 'WikiQA-te
 d_test_iterable = MyWikiIterable('doc', os.path.join(wikiqa_folder, 'WikiQA-test.tsv'))
 l_test_iterable = MyWikiIterable('label', os.path.join(wikiqa_folder, 'WikiQA-test.tsv'))
 
+batch_size = 10
+steps_per_epoch = 20361 // batch_size
 
 kv_model = api.load('glove-wiki-gigaword-300')
 
 # Train the model
 drmm_tks_model = DRMM_TKS(
                     queries=q_iterable, docs=d_iterable, labels=l_iterable, word_embedding=kv_model, epochs=3,
-                    validation_data=[q_val_iterable, d_val_iterable, l_val_iterable], topk=20
+                    topk=20, steps_per_epoch=steps_per_epoch, batch_size=batch_size
                 )
 
 print('Test set results')
