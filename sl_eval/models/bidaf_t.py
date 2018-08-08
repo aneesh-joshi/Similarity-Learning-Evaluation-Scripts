882cfeec6752a053c923ff8d957e713763cd8f4a
aneesh-joshi <aneeshyjoshi@gmail.com>
2018-08-08 19:06:01 +0530

    add random seeds everywhere

--- sl_eval/models/bidaf_t.py
+++ sl_eval/models/bidaf_t.py
@@ -18,6 +18,31 @@ import string
 from keras import optimizers
 
 
+# Set random seed for reproducible results.
+# For more details, read the keras docs:
+# https://keras.io/getting-started/faq/#how-can-i-obtain-reproducible-results-using-keras-during-development
+import os
+os.environ['PYTHONHASHSEED'] = '0'
+# The below is necessary for starting Numpy generated random numbers
+# in a well-defined initial state.
+np.random.seed(42)
+# The below is necessary for starting core Python generated random numbers
+# in a well-defined state.
+rn.seed(12345)
+# Force TensorFlow to use single thread.
+# Multiple threads are a potential source of
+# non-reproducible results.
+# For further details, see: https://stackoverflow.com/questions/42022950/which-seeds-have-to-be-set-where-to-realize-100-reproducibility-of-training-res
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+from keras import backend as K
+# The below tf.set_random_seed() will make random number generation
+# in the TensorFlow backend have a well-defined initial state.
+# For further details, see: https://www.tensorflow.org/api_docs/python/tf/set_random_seed
+tf.set_random_seed(1234)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
+
+
 logger = logging.getLogger(__name__)
 
 class BiDAF_T:
